3e7b4f5733259ce547d8a7f3e60f9280b68b6f43
WispySkies <arpolixgames@gmail.com>
2023-05-19 00:59:34 -0400

    rudimentary tick-based engine, externs for updates
    some comments for future usability

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
+import engine.TickedEngine;
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
+
+        /* ticked alternative */
+        TickedEngine engine = new TickedEngine();
+
+        /* create player objects, render, etc */
+        engine.start();
+
+        /* create external methods for TickedEngine::@35 to update and render */
+
+        //engine.interrupt(); /* for pausing */
+        //engine.run(); /* unpause */
     }
 }
